19d5b915bece483ab531f3adf28842d6a1c25b5c
Roger Faria <roger.faria.dev@outlook.com>
2025-09-02 01:38:06 -0300

    ubuntu-02-07

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./style/theme.css";
+import "./style/global.css";
+import { Heading } from "./components/Heading";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<Heading />
+			<div className="p">
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, neque eos
+				ad dolorum rem nostrum blanditiis earum quos est quo, necessitatibus
+				dolorem et esse sed quia dicta autem tenetur labore!
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/Heading.tsx
@@ -0,0 +1,7 @@
+import styles from "./Heading.module.css";
+
+const Heading = () => {
+	return <h1 className={styles.heading}>Chronos Pomodoro</h1>;
+};
+
+export { Heading };
